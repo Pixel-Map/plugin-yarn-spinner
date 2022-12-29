720db3917759d89cda4a34dbc1b7105c4a2454f4
Ken Erwin <ken@devopslibrary.com>
2022-12-29 17:14:58 -0500

    try uploading artifacts

--- .projenrc.ts
+++ .projenrc.ts
@@ -51,4 +51,38 @@ packageJson?.addOverride('window', {
   icon: 'icon/icon.png',
 });
 
+project.github!.tryFindWorkflow('release')!.file!.addOverride(
+  'jobs.release_github.steps',
+  [
+    { uses: 'actions/setup-node@v3', with: { 'node-version': '14.x' } },
+    { name: 'Download build artifacts', uses: 'actions/download-artifact@v3', with: { name: 'build', path: 'dist' } },
+    {
+      // eslint-disable-next-line
+      name: 'Restore build artifact permissions',
+      // eslint-disable-next-line
+      run: 'cd dist && setfacl --restore=permissions-backup.acl',
+      'continue-on-error': true,
+    },
+    { name: 'Get Tag', run: 'TAG=$(cat dist/releasetag.txt); echo "TAG=$TAG" >> $GITHUB_ENV' },
+    {
+      name: 'Release',
+      uses: 'softprops/action-gh-release@v1',
+      with: {
+        files: ['dist/pixelmapYarnSpinner.js', 'dist/pixelmapYarnSpinner.js.map'],
+        body_path: '${{ github.workspace }}/dist/changelog.md',
+        tag_name: '${{ env.TAG }}',
+      },
+    },
+  ],
+
+  // steps:
+
+  //     - name: Release
+  //       env:
+  //         GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+  //         GITHUB_REPOSITORY: ${{ github.repository }}
+  //         GITHUB_REF: ${{ github.ref }}
+  //       run: errout=$(mktemp); gh release create $(cat dist/releasetag.txt) -R $GITHUB_REPOSITORY -F dist/changelog.md -t $(cat dist/releasetag.txt) --target $GITHUB_REF 2> $errout && true; exitcode=$?; if [ $exitcode -ne 0 ] && ! grep -q "Release.tag_name already exists" $errout; then cat $errout; exit $exitcode; fi
+);
+
 project.synth();
